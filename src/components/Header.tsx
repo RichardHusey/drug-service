96a09cf45493432c21b85fbe35b96977bb74ea98
Richard <richard.eagle1992@gmail.com>
2024-07-10 15:45:23 -0400

    feat: implement Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,130 @@
-import { Box } from "@mui/material";
-
+import { MedicalInformation } from "@mui/icons-material";
+import {
+  AppBar,
+  Box,
+  Button,
+  IconButton,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 const Header = () => {
+  const navigate = useNavigate();
+  const [anchorElNav, setAnchorELNav] = useState<null | HTMLElement>(null);
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorELNav(event.currentTarget);
+  };
+  const handleCloseNavMenu = () => {
+    setAnchorELNav(null);
+  };
+  const handleClickNavMenu = (menuItem: string) => {
+    setAnchorELNav(null);
+    navigate(`/${menuItem}`);
+  };
   return (
-    <Box>
-      <h1>Header Section</h1>
-    </Box>
+    <AppBar position="static">
+      <Toolbar
+        disableGutters
+        sx={{
+          padding: "0 24px",
+          justifyContent: { xs: "space-between", md: "flex-start" },
+        }}
+      >
+        <MedicalInformation
+          sx={{ display: { xs: "none", md: "flex" }, mr: 1 }}
+        />
+        <Typography
+          variant="h6"
+          noWrap
+          component="a"
+          href="#"
+          sx={{
+            mr: 2,
+            display: { xs: "none", md: "flex" },
+            fontFamily: "monospace",
+            fontWeight: 700,
+            letterSpacing: ".3rem",
+            color: "inherit",
+            textDecoration: "none",
+          }}
+        >
+          DrugService
+        </Typography>
+        <Box sx={{ display: { xs: "none", md: "flex" } }}>
+          <Button
+            key={"search"}
+            sx={{ my: 2, color: "white" }}
+            onClick={() => handleClickNavMenu("search")}
+          >
+            Search
+          </Button>
+          <Button
+            key={"detail"}
+            sx={{ my: 2, color: "white" }}
+            onClick={() => handleClickNavMenu("detail")}
+          >
+            Detail
+          </Button>
+        </Box>
+        <Box sx={{ display: { xs: "flex", md: "none" } }}>
+          <IconButton
+            size="large"
+            aria-label="account of current user"
+            aria-controls="menu-appbar"
+            aria-haspopup="true"
+            onClick={handleOpenNavMenu}
+            color="inherit"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Menu
+            anchorEl={anchorElNav}
+            open={Boolean(anchorElNav)}
+            onClose={handleCloseNavMenu}
+            sx={{ display: { xs: "block", md: "none" } }}
+          >
+            <MenuItem
+              key={"drug-search"}
+              onClick={() => handleClickNavMenu("search")}
+            >
+              Drug Search
+            </MenuItem>
+            <MenuItem
+              key={"drug-detail"}
+              onClick={() => handleClickNavMenu("detail")}
+            >
+              Drug Detail
+            </MenuItem>
+          </Menu>
+        </Box>
+        <Box sx={{ display: { xs: "flex", md: "none" }, alignItems: "center" }}>
+          <MedicalInformation
+            sx={{ display: { xs: "flex", md: "none" }, mr: 1 }}
+          />
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              display: { xs: "flex", md: "none" },
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            Drug Service
+          </Typography>
+        </Box>
+      </Toolbar>
+    </AppBar>
   );
 };
 
